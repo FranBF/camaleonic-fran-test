1cb1e6205808fabaa08c6dc97fcff6dfa8a1b114
FranBF <franbenages@outlook.es>
2025-02-01 12:22:37 +0100

    last minute improvements

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,20 +8,20 @@ export default function Dashboard() {
   return (
     <main className="flex flex-col w-full h-full items-center justify-center gap-2 p-2">
       <div className="w-full h-1/2 flex flex-col xl:flex-row gap-2 items-center justify-center">
-        <div className="w-full xl:w-1/2 h-[50vh] bg-[#2B2B2C] rounded-sm flex items-center justify-center flex-col">
+        <div className="w-full xl:w-1/2 h-[50vh] text-center bg-[#2B2B2C] rounded-sm flex items-center justify-center flex-col">
           <h2 className="mt-12 text-[20px] xl:text-[30px]">
             How many todos has each user {`(Scatter Chart)`}
           </h2>
           <ChartScatter></ChartScatter>
         </div>
-        <div className="w-full xl:w-1/2 h-[50vh] flex flex-col items-center justify-center bg-[#2B2B2C] rounded-sm">
+        <div className="w-full xl:w-1/2 h-[50vh] text-center flex flex-col items-center justify-center bg-[#2B2B2C] rounded-sm">
           <h2 className="mt-12 text-[20px] xl:text-[30px]">
             How many todos has each user {`(Bar Chart)`}
           </h2>
           <ChartOne></ChartOne>
         </div>
       </div>
-      <div className="w-full h-1/2 flex flex-col xl:flex-row gap-2 items-center justify-center">
+      <div className="w-full h-1/2 flex flex-col text-center xl:flex-row gap-2 items-center justify-center">
         <div className="w-full xl:w-1/2 h-[50vh] flex flex-col items-center justify-center bg-[#2B2B2C] rounded-sm">
           <h2 className="mt-12 text-[20px] xl:text-[30px]">
             How many todos has each user {`(Line Chart)`}
